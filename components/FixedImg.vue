65a210309c4672d9fe059700f6ba47f87dc7ca37
snozaki <snozaki@netprotections.co.jp>
2022-12-30 14:15:23 +0900

    chore: よく使うボタン、リンクを共通コンポーネント化

---
+++ components/FixedImg.vue
@@ -0,0 +1,17 @@
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+    height="20"
+    width="20"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    src: String,
+    alt: String
+  }
+}
+</script>
---
+++ components/LinkButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn
+    :href="to"
+    text
+    small
+    outlined
+    style="text-transform: none;"
+  >
+    {{ text }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    to: String,
+    text: String
+  }
+}
+</script>
