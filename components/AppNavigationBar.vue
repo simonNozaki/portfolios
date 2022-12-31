6336cbc3d948fe1d966e19d86249ff20dc4520aa
snozaki <snozaki@netprotections.co.jp>
2022-12-31 15:07:52 +0900

    feat: フレックスコンテナのブレークポイント幅を設定

--- components/AppNavigationBar.vue
+++ components/AppNavigationBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar app dense class="teal accent-3">
+  <v-app-bar app dense absolute class="teal accent-3">
     <v-tabs right slider-color="blue-grey darken-4">
       <AppNavigationBarTab text="home" to="/" />
       <AppNavigationBarTab text="works" to="/works" />
