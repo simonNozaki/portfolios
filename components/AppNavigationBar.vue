57ecbf841da7a3ae27eb3213fd0c39140d2dff4c
snozaki <snozaki@netprotections.co.jp>
2022-12-31 16:15:38 +0900

    chore: ナビゲーションのリンクスタイルをカスタム

--- components/AppNavigationBar.vue
+++ components/AppNavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app-bar app dense absolute class="teal accent-3">
-    <v-tabs right slider-color="blue-grey darken-4">
+  <v-app-bar app dense dark flat class="blue-grey darken-4">
+    <v-tabs right slider-color="teal accent-4">
       <AppNavigationBarTab text="home" to="/" />
       <AppNavigationBarTab text="works" to="/works" />
     </v-tabs>
